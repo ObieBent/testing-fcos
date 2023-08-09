1879a72b3d5b2b451319d881e5bc6857f1a23eb4
Boris ASSOGBA <borisassogba@live.fr>
2023-08-09 11:38:58 +0200

    update script

--- clean-terraform-state.sh
+++ clean-terraform-state.sh
@@ -9,7 +9,3 @@ if [[ -d $PWD/terraform/testing-fcos/.terraform || -n "$(find $PWD/terraform/tes
 	rm -f $PWD/terraform.tfstate
 	rm -f $PWD/terraform/testing-fcos/terraform.tfstate*
 fi
-
-if [[ -f /var/lib/libvirt/pool/ssd/testing-fcos.ign ]]; then
-	rm -f /var/lib/libvirt/pool/ssd/testing-fcos.ign
-fi
